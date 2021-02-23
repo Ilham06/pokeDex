ed0dde694ba5926873939101207888645e66f718
ilham <ilhammuhamads6999@gmail.com>
2021-02-23 14:10:07 +0700

    fix bg

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+let headerBg = document.querySelector('.card');
 let content = document.querySelector('.content');
 let aboutContent = document.querySelector('.about-content');
 let aboutContentRight = document.querySelector('.about-content-right');
@@ -29,10 +30,8 @@ let moveList = document.querySelector('.moves-list');
 let moveBox = document.querySelector('.moves-box');
 
 
-
-/**
- * Fetches a pokemon.
- */
+fetchPokemon();
+// fetch pokemon list
 function fetchPokemon() {
     fetch('https://pokeapi.co/api/v2/pokemon?limit=6')
         .then(response => response.json())
@@ -40,47 +39,78 @@ function fetchPokemon() {
             let pokemons = allpokemon.results;
             pokemons.forEach(pokemon => {
                 pokelist.insertAdjacentHTML('beforeend', `
-    			<li onclick=pokeData('${pokemon['url']}')> ${pokemon['name']} </li>`);
+                <li onclick=pokeData('${pokemon['url']}')> ${pokemon['name']} </li>`);
             })
         })
 }
 
-fetchPokemon();
-
-/**
- * { function_description }
- *
- * @param      {<type>}  url     The url
- */
+// fetch pokemon data
 function pokeData(url) {
     fetch(url)
         .then(res => res.json())
         .then(function(pokemon) {
-            // console.log(pokemon);
             let species = pokemon['species']['url'];
-            // console.log(species);
+            let t2 = pokemon['types']['1'];
+            let typeBg = pokemon['types']['0']['type']['name'];
+            switch (typeBg) {
+                case typeBg = 'grass':
+                    headerBg.style.backgroundColor = "green";
+                    break;
+                default:
+                    // statements_def
+                    break;
+            }
+
             name.textContent = pokemon['name'];
             img.src = pokemon['sprites']['front_default'];
             type.textContent = pokemon['types']['0']['type']['name'];
-
-            let t2 = pokemon['types']['1'];
             if (t2) {
                 type2.classList.remove('none');
                 type2.textContent = pokemon['types']['1']['type']['name'];
-            } else 
-            type2.classList.add('none');
-
+            } else
+                type2.classList.add('none');
             id.textContent = pokemon['id'];
             weight.textContent = pokemon['weight'] + 'cm';
             height.textContent = pokemon['height'] + 'kg';
-            ability.textContent = pokemon['abilities']['0']['ability']['name'] + ', ' + pokemon['abilities']['1']['ability']['name']  ;
-
+            ability.textContent = pokemon['abilities']['0']['ability']['name'] + ', ' + pokemon['abilities']['1']['ability']['name'];
             fillHp.style.width = `${pokemon['stats']['0']['base_stat']}%`;
             fillat.style.width = `${pokemon['stats']['1']['base_stat']}%`;
             filldf.style.width = `${pokemon['stats']['2']['base_stat']}%`;
             fillsa.style.width = `${pokemon['stats']['3']['base_stat']}%`;
             fillsd.style.width = `${pokemon['stats']['4']['base_stat']}%`;
             fillspd.style.width = `${pokemon['stats']['5']['base_stat']}%`;
+            fetchSpecies(species);
+
+            // fetch pokemon species
+            function fetchSpecies(species) {
+                fetch(species)
+                    .then(res => res.json())
+                    .then(function(species) {
+                        let evo = species['evolution_chain']['url'];
+                        gen.textContent = species['generation']['name'];
+                        habitat.textContent = species['habitat']['name'];
+                        fetchEvo(evo);
+                    })
+
+            }
+
+            // fetch pokemon evo
+            function fetchEvo(evo) {
+                fetch(evo)
+                    .then(res => res.json())
+                    .then(function(evo) {
+                        let evolv = evo['chain'];
+                        normalForm.textContent = evolv['species']['name'];
+                        if (evolv['evolves_to']['0']) {
+                            evoTypeOne.textContent = evolv['evolves_to']['0']['species']['name'];
+                        } else evoTypeOne.classList.add('none');
+
+                        if (evolv['evolves_to']['0']['evolves_to']['0']) {
+                            document.querySelector('.second-evo').classList.remove('none');
+                            evoTypeTwo.textContent = evolv['evolves_to']['0']['evolves_to']['0']['species']['name'];
+                        } else document.querySelector('.second-evo').classList.add('none');
+                    })
+            }
 
             let moves = pokemon['moves'];
             moves.forEach(move => {
@@ -88,109 +118,46 @@ function pokeData(url) {
                 fetchMove(moveUrl);
             })
 
+            // fetch pokemon move
             function fetchMove(url) {
                 fetch(url)
                     .then(res => res.json())
                     .then(function(move) {
-                        console.log(move);
                         moveList.insertAdjacentHTML('beforeend', `
                         <li class="moves-detail"> ${move['name']} <span>${move['power']}</span> </li>`);
                     })
             }
-
-
-            // fetchEvo(id);
-            fetchSpecies(species);
-
-                function fetchSpecies(species) {
-                    fetch(species)
-                        .then(res => res.json())
-                        .then(function(species) {
-                            let evo = species['evolution_chain']['url'];
-                            // console.log(species['generation']['name']);
-                            // console.log(species['habitat']['name']);
-                            gen.textContent = species['generation']['name'];
-                            habitat.textContent = species['habitat']['name'];
-                            fetchEvo(evo);
-                        })
-
-                }
-
-
-                function fetchEvo(evo) {
-                    fetch(evo)
-                        .then(res => res.json())
-                        .then(function(evo) {
-                            let evolv = evo['chain'];
-                            normalForm.textContent = evolv['species']['name'];
-                            if (evolv['evolves_to']['0']) {
-                                evoTypeOne.textContent = evolv['evolves_to']['0']['species']['name'];
-                            } else evoTypeOne.classList.add('none');
-
-                            if (evolv['evolves_to']['0']['evolves_to']['0']) {
-                                document.querySelector('.second-evo').classList.remove('none');
-                                evoTypeTwo.textContent = evolv['evolves_to']['0']['evolves_to']['0']['species']['name'];
-                            } else document.querySelector('.second-evo').classList.add('none');
-                        })
-                }
         })
 }
-
-
-// about
 
 function about() {
     statContent.classList.add('none');
     evoContent.classList.add('none');
     moveContent.classList.add('none');
-    openAboutContent();
-}
-
-function openAboutContent() {
     aboutContent.classList.remove('none');
     aboutContentRight.classList.remove('none');
 }
-
-
-// stat
 
 function stat() {
     aboutContent.classList.add('none');
     aboutContentRight.classList.add('none');
     evoContent.classList.add('none');
     moveContent.classList.add('none');
-    openStatContent();
-}
-
-function openStatContent() {
     statContent.classList.remove('none');
 }
 
-// evo
 function evo() {
     aboutContent.classList.add('none');
     aboutContentRight.classList.add('none');
     statContent.classList.add('none');
     moveContent.classList.add('none');
-    openEvoContent();
-}
-
-function openEvoContent(id) {
     evoContent.classList.remove('none');
-
 }
 
-// move
 function move() {
     aboutContent.classList.add('none');
     aboutContentRight.classList.add('none');
     statContent.classList.add('none');
     evoContent.classList.add('none');
-    openMoveContent();
-}
-
-function openMoveContent() {
     moveContent.classList.remove('none');
 }
-
-
